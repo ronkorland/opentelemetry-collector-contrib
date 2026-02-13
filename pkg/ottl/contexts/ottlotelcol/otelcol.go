9069073242a9c4bbb3af6fd5f975a310ac3be1ce
edmocosta <11836452+edmocosta@users.noreply.github.com>
2026-02-13 11:03:30 +0100

    make goporto

--- pkg/ottl/contexts/ottlotelcol/otelcol.go
+++ pkg/ottl/contexts/ottlotelcol/otelcol.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package ottlotelcol // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlcontext"
+package ottlotelcol // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlotelcol"
 import (
 	"errors"
 
